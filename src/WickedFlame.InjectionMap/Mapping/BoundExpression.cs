457b3a3b4920fff043b85308ff8644631d8b7840
chriswalpen <c.walpen@gmx.net>
2014-04-17 22:56:55 +0200

    Update

---
+++ src/WickedFlame.InjectionMap/Mapping/BoundExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WickedFlame.InjectionMap.Mapping
+{
+    internal class BoundExpression : BindableComponent, IBoundExpression
+    {
+        public BoundExpression(IComponentContainer container, IMappingComponent component)
+            : base(container, component)
+        {
+        }
+
+        public IMappingOption MappingOption { get; internal set; }
+    }
+}
