71c26c372d21a0e402526004bdd40606d2d763a2
chriswalpen <c.walpen@gmx.net>
2014-11-04 23:02:57 +0100

    Update MapInitializer for named context; Release 1.5.4

--- src/InjectionMap/MapInitializer.cs
+++ src/InjectionMap/MapInitializer.cs
@@ -8,17 +8,34 @@ namespace InjectionMap
 {
     public class MapInitializer : IDisposable
     {
-        readonly IMappingProvider _context;
-
+        readonly IMappingContext _context;
+        
         public MapInitializer()
         {
             _context = MappingContextManager.MappingContext;
         }
 
-        public MapInitializer(IMappingProvider context)
+        public MapInitializer(string context)
         {
+            context.EnsureArgumentNotNullOrEmpty("context");
+            _context = new MappingContext(context);
+        }
+
+        public MapInitializer(IMappingContext context)
+        {
+            context.EnsureArgumentNotNull("context");
             _context = context;
         }
+
+
+        public IMappingContext Context
+        {
+            get
+            {
+                return _context;
+            }
+        }
+
 
         /// <summary>
         /// Initializes all mappings in the <see cref="IMapInitializer"/>
@@ -26,22 +43,8 @@ namespace InjectionMap
         /// <param name="mapper"></param>
         public void Initialize(IMapInitializer mapper)
         {
-            //Initialize(mapper/*, MappingContextManager.MappingContext*/);
             mapper.InitializeMap(_context);
         }
-
-        ///// <summary>
-        ///// Initializes all mappings in the <see cref="IMapInitializer"/>
-        ///// </summary>
-        ///// <param name="mapper"></param>
-        ///// <param name="context">The IMappingProvider to store the mappings</param>
-        //public void Initialize(IMapInitializer mapper, IMappingProvider context)
-        //{
-        //    mapper.EnsureArgumentNotNull("mapper");
-        //    context.EnsureArgumentNotNull("context");
-
-        //    mapper.InitializeMap(context);
-        //}
 
         /// <summary>
         /// Initializes all implementations of <see cref="IMapInitializer"/> in the assembly
@@ -49,20 +52,8 @@ namespace InjectionMap
         /// <param name="assemblyFile"></param>
         public void Initialize(string assemblyFile)
         {
-            Initialize(Assembly.Load(assemblyFile)/*, MappingContextManager.MappingContext*/);
+            Initialize(Assembly.Load(assemblyFile));
         }
-
-        ///// <summary>
-        ///// Initializes all implementations of <see cref="IMapInitializer"/> in the assembly to the provided MappingProvider
-        ///// </summary>
-        ///// <param name="assemblyFile"></param>
-        ///// <param name="context"></param>
-        //public void Initialize(string assemblyFile, IMappingProvider context)
-        //{
-        //    assemblyFile.EnsureArgumentNotNullOrEmpty("assemblyFile");
-
-        //    Initialize(Assembly.Load(assemblyFile), context);
-        //}
 
         /// <summary>
         /// Initializes all implementations of <see cref="IMapInitializer"/> in the assembly
@@ -70,11 +61,8 @@ namespace InjectionMap
         /// <param name="assembly"></param>
         public void Initialize(Assembly assembly)
         {
-            //Initialize(assembly, MappingContextManager.MappingContext);
             assembly.EnsureArgumentNotNull("assembly");
-
-            //InitializeInternal(assembly, _context);
-
+            
             var mappingtype = typeof(IMapInitializer);
             var types = assembly.GetTypes().Where(p => mappingtype.IsAssignableFrom(p) && !p.IsInterface);
 
@@ -90,27 +78,12 @@ namespace InjectionMap
             }
         }
 
-        ///// <summary>
-        ///// Initializes all implementations of <see cref="IMapInitializer"/> in the assembly to the provided MappingProvider
-        ///// </summary>
-        ///// <param name="assembly"></param>
-        ///// <param name="context"></param>
-        //public void Initialize(Assembly assembly, IMappingProvider context)
-        //{
-        //    assembly.EnsureArgumentNotNull("assembly");
-        //    context.EnsureArgumentNotNull("context");
-
-        //    InitializeInternal(assembly, context);
-        //}
-
         /// <summary>
         /// Initializes the <see cref="IMapInitializer"/>
         /// </summary>
         /// <typeparam name="T"></typeparam>
         public void Initialize<T>() where T : IMapInitializer
         {
-            //Initialize<T>(MappingContextManager.MappingContext);
-
             var type = typeof(T);
 
             type.EnsureTypeCanBeDefaultInstantiated();
@@ -120,45 +93,7 @@ namespace InjectionMap
             if (obj != null)
                 obj.InitializeMap(_context);
         }
-
-        ///// <summary>
-        ///// Initializes the <see cref="IMapInitializer"/> to the provided MappingProvider
-        ///// </summary>
-        ///// <typeparam name="T"></typeparam>
-        ///// <param name="context"></param>
-        //public static void Initialize<T>(IMappingProvider context) where T : IMapInitializer
-        //{
-        //    var type = typeof(T);
-
-        //    type.EnsureTypeCanBeDefaultInstantiated();
-        //    type.EnsureTypeIsImplemented(typeof(IMapInitializer));
-
-        //    var obj = Activator.CreateInstance(type) as IMapInitializer;
-        //    if (obj != null)
-        //        obj.InitializeMap(context);
-        //}
-
-        //#region Internal Implementation
-
-        //internal static void InitializeInternal(Assembly assembly, IMappingProvider container)
-        //{
-        //    var mappingtype = typeof(IMapInitializer);
-        //    var types = assembly.GetTypes().Where(p => mappingtype.IsAssignableFrom(p) && !p.IsInterface);
-
-        //    foreach (var type in types)
-        //    {
-        //        type.EnsureTypeCanBeDefaultInstantiated();
-
-        //        var obj = Activator.CreateInstance(type) as IMapInitializer;
-        //        if (obj == null)
-        //            continue;
-
-        //        obj.InitializeMap(container);
-        //    }
-        //}
-
-        //#endregion
-
+        
         #region IDisposeable Implementation
 
         /// <summary>
